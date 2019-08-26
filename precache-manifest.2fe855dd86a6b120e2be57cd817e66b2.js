22323c793a519eda0ee54b1313c26f1aaa6b7a3a
Martin Bagniuk <martin.bagniuk@gmail.com>
2019-08-26 17:06:03 +0300

    Updates

---
+++ precache-manifest.2fe855dd86a6b120e2be57cd817e66b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d777979a83cc080515bda6b73e13fe",
+    "url": "/weather-widget/index.html"
+  },
+  {
+    "revision": "36f7bcb4218c1a2d9b21",
+    "url": "/weather-widget/static/css/main.49a68ddd.chunk.css"
+  },
+  {
+    "revision": "ba6cd377776f64b86421",
+    "url": "/weather-widget/static/js/2.e39558d9.chunk.js"
+  },
+  {
+    "revision": "36f7bcb4218c1a2d9b21",
+    "url": "/weather-widget/static/js/main.d9ad2265.chunk.js"
+  },
+  {
+    "revision": "11cce7114af07730e7c9",
+    "url": "/weather-widget/static/js/runtime~main.90c32d44.js"
+  }
+]);
