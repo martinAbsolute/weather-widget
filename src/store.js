484a2089d4ff2edd97db23ee32fb943f8baac40c
Martin Bagniuk <martin.bagniuk@gmail.com>
2019-08-26 17:39:31 +0300

    Fix redux devtools being a requirement, finalize the app for publishing on gitpages

--- src/store.js
+++ src/store.js
@@ -6,9 +6,10 @@ const initialState = {}
 
 const middlware = [thunk]
 
-const store = createStore(rootReducer, initialState, compose(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, initialState, composeEnhancers(
     applyMiddleware(...middlware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ))
 
 export default store
